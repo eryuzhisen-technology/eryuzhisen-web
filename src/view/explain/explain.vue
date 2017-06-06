d373089bb6bfcd1c9abfcca101fd214aa4da5db3
huangmiao <huangmiao21com@sina.com>
2017-06-06 16:21:32 +0800

    说明页面页面

---
+++ src/view/explain/explain.vue
@@ -0,0 +1,39 @@
+<!-- style样式代码 -->
+<style lang="less">
+@import (less) '../../common/css/common.less';
+@import (less) 'explain.less';
+</style>
+
+<!-- html代码 -->
+<template>
+<div class="app-body">
+	<HeaderDom />
+	<div class="m-explain">
+		<div class="explain-bg">
+			<div class="bg-top"></div>
+			<div class="bg-bottom"></div>
+		</div>
+		<div class="explain-content">
+			<img class="content-bg" src="../../common/images/explain/backgroud.png"/>
+			<div class="content-txt">
+				<p class="text">优秀的装帧</p>
+				<p class="text">传达读者的不仅是你故事的精彩</p>
+				<p class="text">走心的封面</p>
+				<p class="text">公诸于世的不止是你执笔的态度</p>
+				<div class="spacing"></div>
+				<p class="text">品味的作品值得拥有品味的包装</p>
+				<p class="text">于是这里拒绝低俗审美的瞎拼乱凑</p>
+				<p class="text">只有为你精心而设的</p>
+				<p class="text">私人订制</p>
+				<p class="bottom-text">「字数1W以上可申请进入首页并领取定制封面」</p>
+			</div>
+	</div>
+	<FooterDom />
+	<SideMenu />
+</div>
+</template>
+
+<script>
+
+</script>
+
